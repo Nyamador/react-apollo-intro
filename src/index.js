33ab022cecb4f20584448cedc9b4f9e8d45f14df
Nyamador <dselasi4u@gmail.com>
2021-01-04 12:19:26 +0000

    Feat: Configured Apollo

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 // import reportWebVitals from './reportWebVitals';
+import {
+	ApolloProvider,
+	ApolloClient,
+	createHttpLink,
+	InMemoryCache,
+} from '@apollo/client';
+const httpLink = createHttpLink({
+	uri: 'http://localhost:4000',
+});
+
+const client = new ApolloClient({
+	link: httpLink,
+	cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<ApolloProvider client={client}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</ApolloProvider>,
 	document.getElementById('root')
 );
 
